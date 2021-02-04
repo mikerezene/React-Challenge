f5c9c42dd3279862379af827e4d5920fb2d8fba7
mikerezene <mikerezenee@gmail.com>
2021-02-04 21:00:40 +0300

    fix:code_readablity

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 export const Header  = styled.div`
     height: 60px;
-    width: 100%;
+    width: 96.5%;
     padding-left: 10px;
     line-height: 60px;
     border-left: solid 1px black;
-    border-bottom:solid 1px black
+    border-bottom:solid 1px black;
+    border-right: solid 1px black;
 `
--- src/Components/ImageContainer/index.js
+++ src/Components/ImageContainer/index.js
@@ -5,8 +5,8 @@ export const ImageContainer = styled.div`
 height:  "50px";
 width:50px;
 margin-left: ${ props => props.chatBody ? "30px" : "3px"};
-margin-top: ${ props => props.chatBody ? "6px" : "5px"};
-margin-bottom: ${ props => props.chatBody ? "0px" : "3px"};
+margin-top: ${ props => props.chatBody ? "8px" : "5px"};
+margin-bottom: ${ props => props.chatBody ? "2px" : "3px"};
 margin-right:${ props => props.chatBody ? "6px" : "0px"};
 background-image: url(${props => props.image});
 background-size: cover;
